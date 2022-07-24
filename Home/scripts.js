5f83254e6e648f90bb4a675ceeb9d4b135ffd30d
William Silva <tintilho@yahoo.com.br>
2022-07-24 14:44:46 +0100

    atualizacao

--- Home/scripts.js
+++ Home/scripts.js
@@ -4,11 +4,19 @@ const containerSectionOne = document.getElementById('container-section-one')
 const pathContainerSectionTwo = './containers/section-two/index.html'
 const containerSectionTwo = document.getElementById('container-section-two')
 
+const pathContainerSectionThree = './containers/section-three/index.html'
+const containerSectionThree = document.getElementById('container-section-three')
+
 async function onLoadBody() {
   containerSectionOne.innerHTML = await (
     await fetch(pathContainerSectionOne)
   ).text()
+
   containerSectionTwo.innerHTML = await (
     await fetch(pathContainerSectionTwo)
+  ).text()
+
+  containerSectionThree.innerHTML = await (
+    await fetch(pathContainerSectionThree)
   ).text()
 }
